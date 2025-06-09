e01888971d15eeb1ad51bee3779639a4171d5e4f
yevpy <eugene2piskun@gmail.com>
2025-06-09 18:03:54 +0300

    Add custom user with JWT Authentication using email as login, re-create migration for Book model (USE THIS VERSION FOR FUTURE MERGE).

--- library_service/settings.py
+++ library_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "books",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
